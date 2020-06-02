9bc83405584cb5acf8e9c9d3a4e940c917e6d295
Alberto <ansnieto@gmail.com>
2020-06-02 18:51:10 +0300

    vectorlength method implemented

--- gcheck/M01/vector/src/source.c
+++ gcheck/M01/vector/src/source.c
@@ -1,6 +1,14 @@
 #include <math.h>
 #include <stdio.h>
 
+double vectorlength(double x, double y, double z)
+{
+    double x_pow = pow(x, 2);
+    double y_pow  = pow(y, 2);
+    double z_pow = pow(z, 2);
+
+    return sqrt(x_pow + y_pow + z_pow); 
+}
 
 
 
