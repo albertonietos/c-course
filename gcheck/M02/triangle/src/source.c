ed5da193b0c87c40b818af24e33ec6f0e59ba3bf
Alberto <ansnieto@gmail.com>
2020-06-10 11:23:12 +0300

    Implement draw_triangle

--- gcheck/M02/triangle/src/source.c
+++ gcheck/M02/triangle/src/source.c
@@ -4,5 +4,14 @@
 
 void draw_triangle(unsigned int size)
 {
-    
+    for (unsigned int row = size; row > 0; row--) {
+        for (unsigned int col = 0; col < size; col++) {
+            if (row -1 > col){
+                printf(".");
+            } else {
+                printf("#");
+            }
+        }
+        printf("\n");
+    }
 }
