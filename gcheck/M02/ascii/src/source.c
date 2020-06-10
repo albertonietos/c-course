e57ae320adcb056971c0d1c0bd213203c2834c8c
Alberto <ansnieto@gmail.com>
2020-06-10 13:30:40 +0300

    Implement ascii_chart

--- gcheck/M02/ascii/src/source.c
+++ gcheck/M02/ascii/src/source.c
@@ -6,5 +6,17 @@
 
 void ascii_chart(char min, char max)
 {
-    
+    for (int i = min; i <= max; i++) {
+        printf("%3d 0x%x ", i, i);
+        if ( !isprint(i) ) {
+            printf("?");
+        } else {
+            printf("%c", i);
+        }
+        if ((i + 1 - min) % 4 == 0 && i != min) {
+            printf("\n");
+        } else {
+            printf("\t");
+        }
+    }
 }
