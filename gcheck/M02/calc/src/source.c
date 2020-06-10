477a480d4762bc5f97aafa8c075553b10a9fcb91
Alberto <ansnieto@gmail.com>
2020-06-10 09:33:00 +0300

    Implement simple_math

--- gcheck/M02/calc/src/source.c
+++ gcheck/M02/calc/src/source.c
@@ -16,7 +16,40 @@ void simple_multiply(void)
 
 void simple_math(void)
 {
-    
+    float a, b;
+    char op;
+    int ret;
+    ret = scanf("%f %s %f", &a, &op, &b);
+    if (ret > 0) {
+        switch (op)
+        {
+        float c;
+        case '+':
+            c = a + b;
+            printf("%.1f\n", c);
+            break;
+
+        case '-':
+            c = a - b;
+            printf("%.1f\n", c);
+            break;
+        
+        case '*':
+            c = a * b;
+            printf("%.1f\n", c);
+            break;
+
+        case '/':
+            c = a / b;
+            printf("%.1f\n", c);
+            break;
+
+        default:
+            printf("ERR");
+            break;
+        }
+        
+    }
 }
 
 
