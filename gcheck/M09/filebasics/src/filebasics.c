9bc0eba6f178acf9f439cff0e2bb41ca36d8f491
Pasi Sarolahti <pasi.sarolahti@iki.fi>
2020-06-15 21:57:23 +0300

    Module 9 added.

---
+++ gcheck/M09/filebasics/src/filebasics.c
@@ -0,0 +1,23 @@
+#include "filebasics.h"
+
+
+/* Print give file <filename> to the standard output stream.
+ * Return number of characters printed, -1 if file opening fails.
+ */
+int print_file_and_count(const char *filename)
+{
+    
+}
+
+/* Compare two files. The function should return the first lines that differ
+ * in the two files, concatenated together, separated by four dashes, on
+ * different lines. If the files are equal, NULL is returned. 
+ * Function stops immediately, if either one of the files end and returns NULL.
+ * You can assume that lines are not longer than 1000 characters.
+ */
+char *difference(const char *file1, const char *file2)
+{
+    
+}
+
+
---
+++ gcheck/M09/shop/src/shop.c
@@ -0,0 +1,22 @@
+#include "shop.h"
+
+
+int write_binary(const char* filename, const Product* shop)
+{
+	
+}
+
+Product* read_binary(const char* filename)
+{
+	
+}
+
+int write_plaintext(const char* filename, const Product* shop)
+{
+	
+}
+
+Product* read_plaintext(const char* filename)
+{
+	
+}
