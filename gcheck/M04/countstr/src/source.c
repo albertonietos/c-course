57841af6cba555f69342f97c5dad8473fce877a8
Alberto <ansnieto@gmail.com>
2020-06-26 13:13:13 +0300

    Implement num_substr

--- gcheck/M04/countstr/src/source.c
+++ gcheck/M04/countstr/src/source.c
@@ -1,5 +1,5 @@
 #include "source.h"
-
+#include "string.h"
 
 
 /* Count Substring
@@ -8,5 +8,15 @@
  */
 int num_substr(const char *str, const char *sub)
 {
-    
+    int count = 0;
+    char *str_cpy = str;
+    printf("str_copy: %s\n", str_cpy);
+    char *ptr;
+    while (ptr = strstr(str_cpy, sub)) {
+        printf("ptr: %s\n", ptr);
+        count++;
+        str_cpy = ptr + strlen(sub); // move str_cpy pointer to skip the current substring
+        printf("str_copy: %s\n", str_cpy);
+    }
+    return count;
 }
