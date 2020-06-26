2e0251fd68dacca37d88f75d53cee69f00514bda
Alberto <ansnieto@gmail.com>
2020-06-26 14:15:26 +0300

    Implement my_toupper

--- gcheck/M04/shout/src/source.c
+++ gcheck/M04/shout/src/source.c
@@ -1,7 +1,16 @@
 #include "source.h"
-
+#include "ctype.h"
 
 char *my_toupper(char *dest, const char *src)
 {
-	
+	for (int i = 0; *(src + i) != '\0'; i++) {
+        if (*(src + i) == '?') {
+            *(dest + i) = '!';
+        } else if (*(src + i) == '.') {
+            *(dest + i) = '!!!';
+        } else {
+            *(dest + i) = toupper(*(src + i));
+        }
+    }
+    return dest;
 }
