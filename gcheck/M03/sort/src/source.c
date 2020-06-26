33e5d8797c2464004328cd58c93a523131033405
Alberto <ansnieto@gmail.com>
2020-06-26 17:50:10 +0300

    implement sort

--- gcheck/M03/sort/src/source.c
+++ gcheck/M03/sort/src/source.c
@@ -9,5 +9,19 @@
  */
 void sort(int *start, int size)
 {
-    
+    for (int i = 0; i < size; i++) {
+        // Assume the minimum to be in the starting position
+        int min = start[i];
+        int swap_id = i;
+        // Inspect indices starting after i
+        for (int j = i + 1; j < size; j++){
+            if (min > start[j]) {
+                min = start[j];
+                swap_id = j;
+            }
+        }
+    // Swap indices
+    start[swap_id] = start[i];
+    start[i] = min;
+    }
 }
