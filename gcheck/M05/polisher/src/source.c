5ea5ba0c59287303b3e9e3f72724bfa148e2a273
Alberto <ansnieto@gmail.com>
2020-07-03 20:43:07 +0300

    Buggy delete_comments

--- gcheck/M05/polisher/src/source.c
+++ gcheck/M05/polisher/src/source.c
@@ -1,4 +1,4 @@
-
+#include <stdlib.h>
 #include "source.h"
 
 /* Remove C comments from the program stored in memory block <input>.
@@ -8,5 +8,52 @@
  */
 char *delete_comments(char *input)
 {
+    int len = 1;
+    char *copy = input;
     
+    //Get input length
+    while (*copy) {
+        len++;
+        copy++;
+    }
+
+    //Allocate space for text pointer
+    char *text = malloc(len);
+    if (text == NULL) {
+        return NULL;
+    }
+
+    int i = 0;
+    copy = input;
+
+    while (*copy) {
+        if (*copy == '/' && *(copy+1) == '/') {
+            while (*copy && *copy != '\n') {
+                copy++;
+            }
+            copy++;
+        }
+        else if(*copy == '/' && *(copy + 1) == '*') {
+            while (*copy && *copy != '*' && *(copy+1) != '/') {
+                copy++;
+            }
+            copy += 2;
+        }
+        else {
+            text[i] = *copy;
+            i++;
+            copy++;
+        }
+    }
+
+    text[i] = 0;
+    char * new_text = realloc(text, i + 1);
+
+    if (new_text == NULL) {
+        return NULL;
+    }
+
+    free(input);
+    
+    return new_text;
 }
