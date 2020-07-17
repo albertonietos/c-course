db68db4b9231fa0ec41ae466ec077264132689cb
Alberto <ansnieto@gmail.com>
2020-07-17 16:02:00 +0300

    Fix split_string

--- gcheck/M07/strsplit/src/stringsplit.c
+++ gcheck/M07/strsplit/src/stringsplit.c
@@ -2,16 +2,17 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 
-/* Split string by another string, return split parts + NULL in array.
+/* Split string by another string, return split parts + NULL in outputay.
  *
  * Parameters: 
  *  str: the string to split
  *  split: the string to split str with
  *
  * Returns:
- *  A dynamically reserved array of dynamically reserved string parts.
+ *  A dynamically reserved outputay of dynamically reserved string parts.
  *
  * For example called with "Test string split" and " ",
  * returns ["Test", "string", "split", NULL].
@@ -20,19 +21,72 @@
  */
 
 char** split_string(const char *str, const char* split) {
-    char **token;
-    unsigned int i = 0;
+    // Find number of tokens in the string (minimum is 1)
+    unsigned int nbr_tokens = 1;
+    unsigned int len_separator = strlen(split);
+    const char *ptr = strstr(str, split);
 
-    // Get the first token
-    token[i] = strtok(str, split);
-
-    // Look through the other ones
-    while (token[i] != NULL) {
-        token[i] = strtok(NULL, split);
-        i++;
+    while (ptr) {
+        // Advance to next token 
+        ptr = strstr(ptr + len_separator, split);
+        nbr_tokens++;
     }
 
-    return token;
+    // Add one extra to the length for NULL value at the end
+    nbr_tokens++;
+
+    // Allocation of memory for output outputay
+    char **output = malloc(nbr_tokens * sizeof(char *));
+    if (output == NULL) {
+        free(output);
+        return NULL;
+    }
+
+    // Add NULL at the end
+    output[nbr_tokens - 1] = NULL;
+
+    // Set pointer back to start
+    ptr = str;
+
+    // Set new pointer
+    const char *new_ptr = strstr(str, split);
+
+    for (unsigned int i = 0; i < (nbr_tokens - 1); i++) {
+        // Length of token
+        unsigned len = 0;
+
+        // Get length of token to be added
+        if (new_ptr == NULL) {
+            // Last token
+            while (ptr[len]) {
+                len++;
+            }
+        } else {
+            len = new_ptr - ptr;
+        }
+
+        // Allocate memory for this token
+        output[i] = malloc((len + 1) * sizeof(char));
+        if (output[i] == NULL) {
+            for (unsigned int j = 0; j < i; j++) {
+                free(output[j]);
+            }
+            free(output);
+        }
+
+        // Copy the token
+        memcpy(output[i], ptr, len);
+
+        // Assign NULL terminator to the last character
+        output[i][len] = '\0';
+
+        // If there are more tokens left, update ptr & new_ptr to the next ones
+        if (new_ptr != NULL) {
+            ptr = new_ptr + len_separator;
+            new_ptr = strstr(ptr, split);
+        }
+    }
+    return output;
 }
 
 void print_split_string(char **split_string) {
