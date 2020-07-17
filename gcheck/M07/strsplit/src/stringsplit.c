fe3313d19d8e3bde8bd555e2a15b3204a9c6ada0
Alberto <ansnieto@gmail.com>
2020-07-17 13:17:28 +0300

    Implement print_split_spring and faulty split_string

--- gcheck/M07/strsplit/src/stringsplit.c
+++ gcheck/M07/strsplit/src/stringsplit.c
@@ -1,6 +1,7 @@
 #include "stringsplit.h"
 
 #include <stdio.h>
+#include <string.h>
 
 
 /* Split string by another string, return split parts + NULL in array.
@@ -19,12 +20,25 @@
  */
 
 char** split_string(const char *str, const char* split) {
-    
-    return NULL;
+    char **token;
+    unsigned int i = 0;
+
+    // Get the first token
+    token[i] = strtok(str, split);
+
+    // Look through the other ones
+    while (token[i] != NULL) {
+        token[i] = strtok(NULL, split);
+        i++;
+    }
+
+    return token;
 }
 
 void print_split_string(char **split_string) {
-    
+    for (int i = 0; split_string[i] != NULL; i++) {
+        printf("%s\n", split_string[i]);
+    }
 }
 
 void free_split_string(char **split_string) {
