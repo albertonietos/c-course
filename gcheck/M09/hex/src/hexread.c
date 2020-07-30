085265638888d1a68639a005a38484b895bbf383
Alberto Nieto <ansnieto@gmail.com>
2020-07-30 21:24:49 +0300

    Implement method to print file as hexdump

--- gcheck/M09/hex/src/hexread.c
+++ gcheck/M09/hex/src/hexread.c
@@ -1,5 +1,6 @@
 #include "hexread.h"
-
+#include <stdio.h>
+#include <stdlib.h>
 
 /* Prints the given file as hexdump, at most 16 numbers per line.
  * <filename> parameter is the name of the file.
@@ -8,5 +9,32 @@
  */
 int file_to_hex(const char *filename)
 {
-    
+    // Open file for reading
+    FILE *file = fopen(filename, "r");
+    if (!file) {
+        return -1;
+    }
+
+    // Read the 1st element of file and store it in ptr
+    unsigned int count = 0;
+    unsigned char *ptr = malloc(sizeof(char));
+    size_t nmemb = 1;
+    size_t n = fread(ptr, sizeof(unsigned char), nmemb, file);
+    count++;
+
+    while (n == nmemb) {
+        printf("%02x ", *ptr);
+        
+        // Jump line after 16 hex values
+        if (count % 16 == 0){
+            printf("\n");
+        }
+        count++;
+
+        // Read next element in file
+        n = fread(ptr, sizeof(unsigned char), nmemb, file);
+    }
+
+    free(ptr);
+    return 0;
 }
