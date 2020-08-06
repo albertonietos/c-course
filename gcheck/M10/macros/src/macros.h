94dc1754dd21b1381f9fc105895d38801716da93
Alberto Nieto <ansnieto@gmail.com>
2020-08-06 11:00:19 +0300

    Implement macros

--- gcheck/M10/macros/src/macros.h
+++ gcheck/M10/macros/src/macros.h
@@ -2,7 +2,7 @@
 #define AALTO_MACROS_H
 
 // Implement something here...
+#define EQ3(a,b,c) (((a == b) && (b == c) && (a == c)) ? 1 : 0)
 
-
-
+#define MIN3(a,b,c) (((a < b) && (a < c)) ? a : ((b < c) ? b : c))
 #endif
--- gcheck/M10/macros/src/main.c
+++ gcheck/M10/macros/src/main.c
@@ -7,7 +7,7 @@ int main(void)
 {
 #ifdef EQ3
     printf("Testing EQ3:\n");
-    printf("EQ3(3,3,3) returned %d, should have been 1.\n", EQ3(3,3,3));
+    printf("5*EQ3(1.2f,1.2f,1.2f) returned %d, should have been 5.\n", 5*EQ3(1.2f,1.2f,1.2f));
     printf("EQ3(1,2,3) returned %d, should have been 0.\n", EQ3(1,2,3));
 #else
     printf("Implement EQ3 first");
@@ -16,6 +16,7 @@ int main(void)
 #ifdef MIN3 
     printf("\nTesting MIN3:\n");
     printf("MIN3(1,2,3) returned %d, should have been 1.\n", MIN3(1,2,3));
+    printf("10*MIN3(2,1,2) returned %d, should have been 10.\n", 10*MIN3(2,1,2));
 #else
     printf("Implement MIN3 first");
 #endif    
