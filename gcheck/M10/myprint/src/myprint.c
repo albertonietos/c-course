41aabb3fdff5962ccc08a9512a53fa5fe0ef0e73
Alberto Nieto <ansnieto@gmail.com>
2020-08-07 14:20:28 +0300

    Implement variable length argument lists

--- gcheck/M10/myprint/src/myprint.c
+++ gcheck/M10/myprint/src/myprint.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
 
+int myprint(const char *str, ...) {
+    int count = 0;
+    va_list args;
+    va_start(args, str);
+
+    for (unsigned int i = 0; i < strlen(str); i++) {
+        // If character is ampersan -> print integer
+        if (str[i] != '&') {
+            fputc(str[i], stdout);
+        } else {
+            printf("%d", va_arg(args, int));
+            count++;
+        }
+    }
+    va_end(args);
+
+    return count;
+}
